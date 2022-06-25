86ea267b5ff6748580b5cf98410bf695ec08ae2c
Marcio Rocha <dev.marcio.rocha@gmail.com>
2022-06-25 18:52:38 -0300

    finalizado service para calculadora

--- src/app/calculadora/services/calculadora.service.ts
+++ src/app/calculadora/services/calculadora.service.ts
@@ -5,5 +5,35 @@ import { Injectable } from '@angular/core';
 })
 export class CalculadoraService {
 
+  // operações matemáticas 
+  // definidas em constantes
+  static readonly SOMA: string = '+';
+  static readonly SUBTRACAO: string = '-';
+  static readonly MULTIPLICACAO: string = '*';
+  static readonly DIVISAO: string = '/';
+
   constructor() { }
+
+  calcular(n1: number, n2: number, op: string): number{
+    let resultado: number;
+    switch(op){
+      case CalculadoraService.SOMA:
+        resultado = n1+n2;
+        break;
+      case CalculadoraService.SUBTRACAO:
+        resultado = n1-n2;
+        break;
+      case CalculadoraService.MULTIPLICACAO:
+        resultado = n1 - n2;
+        break;
+      case CalculadoraService.DIVISAO:
+        resultado = n1 / n2;
+        break;
+      default:
+        resultado = 0;
+        break;
+    }
+    return resultado;
+  }
+
 }
