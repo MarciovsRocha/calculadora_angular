f244c26a2dda3589c73c7dbb34b4fab447f09ff2
Marcio Rocha <dev.marcio.rocha@gmail.com>
2022-06-25 09:39:55 -0300

    criado modulo e component calculadora

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+import { CalculadoraComponent } from './calculadora';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CalculadoraComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
